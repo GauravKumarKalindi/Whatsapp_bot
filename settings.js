d2590adee467c36f7270be0ddf690496dd503d55
Gaurav Kumar Kalindi <122034185+GauravKumarKalindi@users.noreply.github.com>
2025-02-08 19:01:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Gaurav', // Your name
+  ownerNumber: '918340626058', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
